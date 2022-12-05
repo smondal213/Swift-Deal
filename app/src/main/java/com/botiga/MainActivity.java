c9347d7b24ca6b013944062065a3653d31738abf
smondal213 <119871443+smondal213@users.noreply.github.com>
2022-12-05 20:18:04 +0530

    project upload

---
+++ app/src/main/java/com/botiga/MainActivity.java
@@ -0,0 +1,27 @@
+package com.botiga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String SITE_URL = "http://ecom2022.liveblog365.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        webView.loadUrl(SITE_URL);
+    }
+}
